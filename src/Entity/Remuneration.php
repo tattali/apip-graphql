aa4d6949dcbcb3e1b5959a086a0e07533cdaa042
Théo Attali <theoattali@gmail.com>
2020-11-10 12:29:44 +0100

    remove unneeded phpdoc

--- src/Entity/Remuneration.php
+++ src/Entity/Remuneration.php
@@ -21,17 +21,11 @@ class Remuneration
      */
     private $fullPackage;
 
-    /**
-     * @codeCoverageIgnore
-     */
     public function getFixedSalary(): ?int
     {
         return $this->fixedSalary;
     }
 
-    /**
-     * @codeCoverageIgnore
-     */
     public function setFixedSalary(?int $fixedSalary): self
     {
         $this->fixedSalary = $fixedSalary;
@@ -39,17 +33,11 @@ class Remuneration
         return $this;
     }
 
-    /**
-     * @codeCoverageIgnore
-     */
     public function getFullPackage(): ?int
     {
         return $this->fullPackage;
     }
 
-    /**
-     * @codeCoverageIgnore
-     */
     public function setFullPackage(?int $fullPackage): self
     {
         $this->fullPackage = $fullPackage;
